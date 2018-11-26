24eee8692c743d3f36da21fa11c89b2a3a8f8457
Sosuke Matsui <e34975@jp.ibm.com>
2018-11-26 23:48:02 +0900

    Adding addition and subtraction

--- 9cc.c
+++ 9cc.c
@@ -3,14 +3,34 @@
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    fprintf(stderr, "Invalid numer of arguments\n");
+    fprintf(stderr, "Invalid number of arguments\n");
     return 1;
   }
+
+  char *p = argv[1];
 
   printf(".intel_syntax noprefix\n");
   printf(".global _main\n");
   printf("_main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+  printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "Unexpected character: '%c'\n", *p);
+    return 1;
+  }
+
   printf("  ret\n");
   return 0;
 }
