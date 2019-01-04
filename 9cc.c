8b48c430e44aeaa6ba138d928a224973a3c574ee
Sosuke Matsui <e34975@jp.ibm.com>
2019-01-05 07:10:07 +0900

    Added syntax tree and code generator

--- 9cc.c
+++ 9cc.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 
 // Token type
 enum {
@@ -15,9 +16,23 @@ typedef struct {
   char *input;  //for error message
 } Token;
 
+enum {
+  ND_NUM = 256,
+};
+
+typedef struct Node {
+  int ty;
+  struct Node *lhs;
+  struct Node *rhs;
+  int val;
+} Node;
+
 // Buffer for tokenized result.
 // Assumption: the max number of tokens are 100.
 Token tokens[100];
+
+// Index of tokens
+int pos;
 
 void tokenize(char *p) {
   int i = 0;
@@ -51,10 +66,76 @@ void tokenize(char *p) {
   tokens[i].input = p;
 }
 
-void error(int i) {
-  fprintf(stderr, "Unexpected error: %s\n",
-          tokens[i].input);
+void error(const char *format, ...) {
+  va_list args;
+  va_start(args, format);
+  vfprintf(stderr, format, args);
+  va_end(args);
   exit(1);
+}
+
+Node *new_node(int ty, Node *lhs, Node *rhs) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = ty;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *term(){
+  if (tokens[pos].ty == TK_NUM) {
+    return new_node_num(tokens[pos++].val);
+  }
+  error("not a number or a left parenthesis: %s", tokens[pos].input);
+}
+
+Node *mul(){
+  Node *lhs = term();
+  return lhs;
+}
+
+Node *expr(){
+  Node *lhs = mul();
+  if (tokens[pos].ty == '+'){
+    pos++;
+    return new_node('+', lhs, expr());
+  }
+  if (tokens[pos].ty == '-'){
+    pos++;
+    return new_node('-', lhs, expr());
+  }
+  return lhs;
+}
+
+void gen(Node *node){
+  if(node->ty == ND_NUM){
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch(node->ty){
+  case '+':
+    printf("  add rax, rdi\n");
+    break;
+  case '-':
+    printf("  sub rax, rdi\n");
+    break;
+  }
+
+  printf("  push rax\n");
 }
 
 int main(int argc, char **argv) {
@@ -64,40 +145,16 @@ int main(int argc, char **argv) {
   }
 
   tokenize(argv[1]);
+  Node *node = expr();
 
   printf(".intel_syntax noprefix\n");
   printf(".global _main\n");
   printf("_main:\n");
 
-  if (tokens[0].ty != TK_NUM)
-    error(0);
-  printf("  mov rax, %d\n", tokens[0].val);
+  gen(node);
 
-  int i = 1;
-  while (tokens[i].ty != TK_EOF) {
-    if (tokens[i].ty == '+') {
-      i++;
-      if (tokens[i].ty != TK_NUM) {
-        error(i);
-      }
-      printf("  add rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-
-    if (tokens[i].ty == '-') {
-      i++;
-      if (tokens[i].ty != TK_NUM) {
-        error(i);
-      }
-      printf("  sub rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-
-    error(i);
-  }
-
+  printf("  pop rax\n");
   printf("  ret\n");
+
   return 0;
 }
